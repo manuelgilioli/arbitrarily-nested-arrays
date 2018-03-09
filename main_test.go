ecb52096bca9b67158a0ad09b0c76e05dfcad600
manuelgilioli <m.gilioli@axwave.com>
2018-03-09 11:27:44 -0800

    add test

--- main_test.go
+++ main_test.go
@@ -3,10 +3,9 @@ package main
 import "testing"
 
 func TestFlat(t *testing.T) {
-	output := []int{1, 2, 3, 4}
-	var result []int
-	//root sample of composite array [1,[2,3],4]
-	var root obj
+	output := []int{1, 2, 3, 4} //output expected
+	var result []int            //result of flat function
+	var root obj                //input nested array
 	root.next = make([]obj, 3)
 	root.next[0].value = 1
 	root.next[1].next = make([]obj, 2)
@@ -14,7 +13,6 @@ func TestFlat(t *testing.T) {
 	root.next[1].next[0].value = 2
 	root.next[1].next[1].value = 3
 	flat(&result, &root)
-
 	if len(result) != len(output) {
 		t.Error("Array lenght", len(result),
 			"expected", len(output))
